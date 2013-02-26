f0b3590bc500baa8c7525c94cd31ea41c3e788c3
Ruben Vincenten <rubenvincenten@gmail.com>
2013-02-26 22:43:14 +0100

    Add tekDebug

--- functions.lua
+++ functions.lua
@@ -1,2 +1,17 @@
 local addonName, ns = ...
 local oUF = oUF
+local strformat = string.format
+
+local nofunc = function() end
+
+--[[
+	Debug function
+]]
+do
+	local debugf = tekDebug and tekDebug:GetFrame(addonName)
+	if debugf then
+		ns.Debug = function(msg, ...) debugf:AddMessage(strformat(msg, ...)) end
+	else
+		ns.Debug = nofunc
+	end
+end
