ee2a2582647bef94eada863c67e8ddc2ddb6d272
Ruben Vincenten <rubenvincenten@gmail.com>
2013-02-27 00:58:04 +0100

    Add some frame sizes

--- config.lua
+++ config.lua
@@ -2,6 +2,29 @@ local addonName, ns = ...
 local oUF = oUF
 
 local config = {
+	statusbar = [[Interface\TargetingFrame\UI-StatusBar]],
+	font = GameFontNormal:GetFont(),
+
+	big = { w = 200, h = 40},
+	normal = { w = 150, h = 40},
+	small = { w = 80, h = 40},
+	tiny = { w = 40, h = 40},
+
+	type2size = {
+		player = 'big',
+		target = 'big',
+		focus = 'big',
+		party = 'big',
+
+		partypet = 'small',
+		partytarget = 'small',
+		raid15 = 'small',
+
+		raid25 = 'tiny',
+		raid40 = 'tiny',
+
+		-- the rest of the frames will get normal size (such as pet)
+	}
 }
 
 ns.config = config
